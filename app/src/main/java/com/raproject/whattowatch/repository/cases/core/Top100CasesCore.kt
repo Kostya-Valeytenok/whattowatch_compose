5d59825af7ecc25adbcea222e0db5197cc651c87
k.valeitsenak <reus.ainceborn7127790>
2022-02-17 14:07:57 +0300

    add Top100Cases with Core

---
+++ app/src/main/java/com/raproject/whattowatch/repository/cases/core/Top100CasesCore.kt
@@ -0,0 +1,9 @@
+package com.raproject.whattowatch.repository.cases.core
+
+import com.raproject.whattowatch.repository.cases.core.base.BaseContentCaseCore
+import com.raproject.whattowatch.utils.ContentType
+import org.koin.core.KoinComponent
+
+class Top100CasesCore : KoinComponent, BaseContentCaseCore() {
+    override val contentType: ContentType get() = ContentType.Top100
+}
