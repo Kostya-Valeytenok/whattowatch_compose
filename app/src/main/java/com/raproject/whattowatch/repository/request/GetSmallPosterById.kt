21ebd3ee5169f1801e35d75b70ad61857eeafca5
Kanstantin Valeitsenak <kanstantsin.valeitsenak@leverx.com>
2022-07-04 16:47:43 +0300

    add GetSmallPosterById request

---
+++ app/src/main/java/com/raproject/whattowatch/repository/request/GetSmallPosterById.kt
@@ -0,0 +1,30 @@
+package com.raproject.whattowatch.repository.request
+
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+
+class GetSmallPosterById(private val params: Bundle) : GetRequest<Bitmap>() {
+
+    companion object {
+        private const val CONTENT_ID = "CONTENT_ID"
+
+        fun createParams(contentId: String): Bundle =
+            Bundle().apply { putString(CONTENT_ID, contentId) }
+    }
+
+    private val contentId: String
+        get() = params.getString(CONTENT_ID, "")
+
+
+    override suspend fun SQLiteDatabase.runRequest(): Bitmap  {
+        val key = portersTable.key
+        val table = portersTable.tableName()
+        val field = portersTable.Image.name
+
+        return safeGetRequest("SELECT $field FROM $table WHERE $key = $contentId") {
+            BitmapFactory.decodeByteArray(this.getBlob(0), 0, this.getBlob(0)!!.size)
+        }
+    }
+}
