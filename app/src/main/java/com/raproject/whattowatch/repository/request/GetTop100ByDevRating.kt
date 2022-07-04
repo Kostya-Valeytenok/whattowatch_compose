39b753439db09f4d74e4b61417737d7fdbbf7076
Kanstantin Valeitsenak <kanstantsin.valeitsenak@leverx.com>
2022-07-04 16:47:56 +0300

    add GetTop100ByDevRating request

---
+++ app/src/main/java/com/raproject/whattowatch/repository/request/GetTop100ByDevRating.kt
@@ -0,0 +1,39 @@
+package com.raproject.whattowatch.repository.request
+
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import com.raproject.whattowatch.utils.DBTable
+import com.raproject.whattowatch.utils.Localization
+
+class GetTop100ByDevRating(params:Bundle) : GetRequest<String>() {
+
+
+    companion object {
+        private const val LOCALIZATION_KEY = "LOCALIZATION_KEY"
+        private const val ORDER_COMMAND = "ORDER_TYPE"
+
+        fun createParams(
+            localization: Localization,
+            orderCommand: String
+        ): Bundle = Bundle().apply {
+            putString(ORDER_COMMAND, orderCommand)
+            putSerializable(LOCALIZATION_KEY, localization)
+        }
+    }
+
+    private val localization: Localization by lazy { params.getSerializable(LOCALIZATION_KEY) as Localization }
+    private val orderCommand: String by lazy { params.getString(ORDER_COMMAND, "") }
+
+    override suspend fun SQLiteDatabase.runRequest(): String {
+        val contentTable = mainTable.tableName(localization)
+        return safeGetRequest("select ${DBTable.MainTable.key} from $contentTable$orderCommand LIMIT 100") {
+            val idList = mutableListOf<String>()
+            while (!isAfterLast) {
+                idList.add(getString(0))
+                toNext()
+            }
+            idList.joinToString()
+        }
+    }
+
+}
