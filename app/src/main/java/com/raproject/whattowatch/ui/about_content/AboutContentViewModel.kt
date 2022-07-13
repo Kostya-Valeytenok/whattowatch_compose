2b9b6cb1702d5a770bec802ce93bda12a6debce6
Kanstantin Valeitsenak <kanstantsin.valeitsenak@leverx.com>
2022-07-13 19:48:20 +0300

    AboutContentViewModel refactor

--- app/src/main/java/com/raproject/whattowatch/ui/about_content/AboutContentViewModel.kt
+++ app/src/main/java/com/raproject/whattowatch/ui/about_content/AboutContentViewModel.kt
@@ -1,27 +1,51 @@
 package com.raproject.whattowatch.ui.about_content
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.raproject.whattowatch.models.ContentDetailsStatus
+import com.raproject.whattowatch.repository.use_case.AddContentToFavoriteUseCase
+import com.raproject.whattowatch.utils.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.koin.core.component.inject
 
-class AboutContentViewModel : ViewModel() {
+class AboutContentViewModel : BaseViewModel() {
 
     private val contentMutableState = MutableStateFlow(ContentDetailsStatus.onLoading)
 
-    val contentState : StateFlow<ContentDetailsStatus>
+    private val addToFavoriteUseCase: AddContentToFavoriteUseCase by inject()
+
+    private val isInFavoriteMutableStateFlow = MutableStateFlow(false)
+
+    val isInFavoriteState: StateFlow<Boolean>
+        get() = isInFavoriteMutableStateFlow
+
+    val contentState: StateFlow<ContentDetailsStatus>
         get() = contentMutableState
 
-    fun addToFavorite(){
-        contentMutableState.value.modelOrNull()?.let {  }
+    fun manageFavoriteStatus(onErrorAction:  (Throwable) -> Unit) = viewModelScope.launch(Dispatchers.Default) {
+        contentMutableState.value.modelOrNull()?.let {
+            if (isInFavoriteState.value.not()) addToFavorite(
+                contentId = it.id,
+                onError = onErrorAction
+            )
+            else removeFromFavorite(contentId = it.id, onError = onErrorAction)
+        }
     }
 
-    fun RemoveFromFavorite(){
-        contentMutableState.value.modelOrNull()?.let {  }
+    private suspend fun addToFavorite(contentId: String, onError: (Throwable) -> Unit) {
+        addToFavoriteUseCase.invoke(value = contentId)
+            .onSuccess { isInFavoriteMutableStateFlow.emit(true) }
+            .onFailure(onError)
+    }
+
+    private suspend fun removeFromFavorite(contentId: String, onError: (Throwable) -> Unit) {
+        contentMutableState.value.modelOrNull()?.let { }
     }
 
 
-    private fun ContentDetailsStatus.modelOrNull() : ContentDetailsStatus.ContentInformationModel?{
-       return if(this is ContentDetailsStatus.ContentInformationModel)  this else null
+    private fun ContentDetailsStatus.modelOrNull(): ContentDetailsStatus.ContentInformationModel? {
+        return if (this is ContentDetailsStatus.ContentInformationModel) this else null
     }
 }
